f1f31d2ba3a8102b188563da98e59ac5db1fd3a2
Jackson Lessa <jacksonj.lessa@gmail.com>
2022-09-14 10:12:54 -0300

    create product add mock test

---
+++ src/modules/product-adm/usecase/add-product/add-product.dto.ts
@@ -0,0 +1,20 @@
+import { string } from "yup";
+
+export interface AddProductInputDto{
+  id?:string;
+  name: string;
+  description: string;
+  purchasePrice: number;
+  stock: number;
+}
+
+
+export interface AddProductOutputDto {
+  id:string;
+  name: string;
+  description: string;
+  purchasePrice: number;
+  stock: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
