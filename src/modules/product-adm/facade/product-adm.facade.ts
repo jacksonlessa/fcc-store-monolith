463e2ee7e9d4ac2f4de05f99fb6bdeba1f72c18b
Jackson Lessa <jacksonj.lessa@gmail.com>
2022-09-15 13:47:58 -0300

    facade to interact with product-adm usecase

---
+++ src/modules/product-adm/facade/product-adm.facade.ts
@@ -0,0 +1,29 @@
+import UseCaseInterface from "../../@shared/usecase/use-case.interface";
+import ProductAdmFacadeInterface, {
+  AddProductFacadeInputDto, 
+  CheckStockFacadeInputDto, 
+  CheckStockFacadeOutputDto
+} from "./product-adm.facade.interface";
+
+export interface UseCasesProps{
+  addUseCase: UseCaseInterface;
+  stockUseCase: UseCaseInterface;
+}
+
+export default class ProductAdmFacade implements ProductAdmFacadeInterface{
+
+  private _addUseCase: any;
+  private _checkStockUseCase: any;
+
+  constructor(useCasesProps: UseCasesProps){
+    this._addUseCase = useCasesProps.addUseCase;
+    this._checkStockUseCase = useCasesProps.stockUseCase;
+  }
+
+  addProduct(input: AddProductFacadeInputDto): Promise<void> {
+    return this._addUseCase.execute(input);
+  }
+  checkStock(input: CheckStockFacadeInputDto): Promise<CheckStockFacadeOutputDto> {
+    return this._checkStockUseCase.execute(input);
+  }
+}
